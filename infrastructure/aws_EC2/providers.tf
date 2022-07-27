a155c0eab449b6cfa82d97f1f197676e9f9585cf
Sebastian Blum <sebastian.blum@alexanderthamm.com>
2022-07-27 19:56:50 +0200

    added providers.tf

---
+++ infrastructure/aws_EC2/providers.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = var.region
+  # access_key = var.aws_access_key_id
+  # secret_key = var.aws_secret_access_key
+  profile = "seblum"
+}
+
+provider "docker" {
+  registry_auth {
+    address  = local.aws_ecr_url
+    username = data.aws_ecr_authorization_token.token.user_name
+    password = data.aws_ecr_authorization_token.token.password
+  }
+}
+
