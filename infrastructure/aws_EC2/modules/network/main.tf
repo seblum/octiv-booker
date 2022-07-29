bdc07a631ba62b7b78dc4cb74d744403b7e5e76a
Sebastian Blum <sebastian.blum@alexanderthamm.com>
2022-07-29 12:51:08 +0200

    modified network

--- infrastructure/aws_EC2/modules/network/main.tf
+++ infrastructure/aws_EC2/modules/network/main.tf
@@ -1,31 +1,31 @@
 
-resource "aws_vpc" "octivbooker-vpc" {
-  cidr_block           = var.cidr_vpc
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-}
+# resource "aws_vpc" "octivbooker-vpc" {
+#   cidr_block           = var.cidr_vpc
+#   enable_dns_hostnames = true
+#   enable_dns_support   = true
+# }
 
-resource "aws_subnet" "octivbooker-subnet" {
-  vpc_id     = aws_vpc.octivbooker-vpc.id
-  cidr_block = var.cidr_subnet
-}
+# resource "aws_subnet" "octivbooker-subnet" {
+#   vpc_id     = aws_vpc.octivbooker-vpc.id
+#   cidr_block = var.cidr_subnet
+# }
 
-resource "aws_internet_gateway" "octivbooker-ig" {
-  vpc_id = aws_vpc.octivbooker-vpc.id
-}
+# resource "aws_internet_gateway" "octivbooker-ig" {
+#   vpc_id = aws_vpc.octivbooker-vpc.id
+# }
 
-resource "aws_route_table" "octivbooker-rt" {
-  vpc_id = aws_vpc.octivbooker-vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.octivbooker-ig.id
-  }
-}
+# resource "aws_route_table" "octivbooker-rt" {
+#   vpc_id = aws_vpc.octivbooker-vpc.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.octivbooker-ig.id
+#   }
+# }
 
-resource "aws_route_table_association" "octivbooker-rta" {
-  subnet_id      = aws_subnet.octivbooker-subnet.id
-  route_table_id = aws_route_table.octivbooker-rt.id
-}
+# resource "aws_route_table_association" "octivbooker-rta" {
+#   subnet_id      = aws_subnet.octivbooker-subnet.id
+#   route_table_id = aws_route_table.octivbooker-rt.id
+# }
 
 
 resource "aws_security_group" "octivbooker-sg" {
