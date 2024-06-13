4cb4382006961000ce958d4171020e0b504dc7c1
Sebastian Blum <sebast.blum@gmail.com>
2024-06-13 20:21:31 +0200

    feat: remove infrastructure dir

--- infrastructure/aws_EC2/modules/network/output.tf
+++
@@ -1,9 +0,0 @@
-output "aws_security_group" {
-  value       = aws_security_group.octivbooker-sg.id
-  description = "AWS security group of the given vpc network"
-}
-
-# output "aws_subnet" {
-#   value       = aws_subnet.octivbooker-subnet.id
-#   description = "AWS subnet of the given vpc network"
-# }
