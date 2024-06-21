2f041c214954da92f333dd5b116685430a9a1238
Sebastian Blum <sebastian.blum@1komma5grad.com>
2024-06-21 10:34:25 +0200

    feat: add pre-commit hook to fetch version

--- .github/scripts/update_chromedriver_version.sh
+++ .github/scripts/update_chromedriver_version.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+# Read the arguments passed to the script
+DOCKERFILE=$1
+
+if [ -z "$DOCKERFILE" ]; then
+    echo "Usage: $0 <Dockerfile>"
+    exit 1
+fi
+
 # Ensure jq is installed
 if ! command -v jq &> /dev/null
 then
@@ -11,7 +19,6 @@ fi
 # Fetch the latest version number from GoogleChromeLabs
 LATEST_VERSION=$(curl -s https://googlechromelabs.github.io/chrome-for-testing/latest-versions-per-milestone.json | jq -r '.milestones | to_entries | map(.value.version) | max')
 
-# Update the Dockerfile with the latest version
 sed -i '' -e "s/ENV ChromedriverVersion=\"[^\"]*\"/ENV ChromedriverVersion=\"$LATEST_VERSION\"/" "$DOCKERFILE"
 
 echo "Updated $DOCKERFILE with ChromeDriver version $LATEST_VERSION"
--- poetry.Dockerfile
+++ poetry.Dockerfile
@@ -14,7 +14,7 @@ ENV TZ="Europe/Berlin"
 # Install Chromedriver
 RUN apt-get install -yqq unzip
 # https://googlechromelabs.github.io/chrome-for-testing/#stable
-ENV ChromedriverVersion="126.0.6478.55"
+ENV ChromedriverVersion="128.0.6548.0"
 # $ChromedriverVersion
 RUN wget -O /tmp/chromedriver.zip https://storage.googleapis.com/chrome-for-testing-public/$ChromedriverVersion/linux64/chromedriver-linux64.zip
 # https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$ChromedriverVersion/linux64/chromedriver-linux64.zip
